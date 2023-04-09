10dbfcdbe86f00b761aac245142b384bb201b5eb
Aryadeep Chakraborty <aryadeepit@gmail.com>
2023-04-09 20:07:12 +0530

    SmartChat v1.0

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from './routes/openai-OLD.js';
+import authRoutes from './routes/auth.js';
+
+/* CONFIG */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+/* OPENAI */
+// const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
+
+/* ROUTES */
+app.use('/openai', openAiRoutes);
+app.use('/auth', authRoutes);
+
+
+/* SERVER */
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {console.log(`Example app listenning at http://localhost:${PORT}`);
+});
